d63fd0c67a0904e404a8e36868c8e22f182e924a
Anuj-Parihar <anujparihar0001@gmail.com>
2025-10-05 03:15:15 +0530

    update

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const cors = require("cors");
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: ['https://www.protaskssolution.com','https://protaskssolution.com']
-}));
+// app.use(cors({
+//   origin: ['https://www.protaskssolution.com','https://protaskssolution.com']
+// }));
+app.use(cors());
 // app.use(bodyParser.json());
 app.use(express.json());
 // MongoDB Connection
