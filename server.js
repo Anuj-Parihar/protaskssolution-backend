5f21a5b946a83dd65004ea3c3299e0d2146717f3
Anuj-Parihar <anujparihar0001@gmail.com>
2025-10-07 03:27:03 +0530

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors({
   origin: ['https://www.protaskssolution.com','https://protaskssolution.com']
 }));
 // app.use(cors());
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(express.json());
 // MongoDB Connection
 mongoose
